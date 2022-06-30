a459f65dd919d1eea7f9ad8fed62ebc46796acc1
Chaudhari <sushant.chaudhari@capgemini.com>
2022-06-30 16:50:52 +0530

    Operations

--- src/main/java/com/example/sushant/Endpoint/UserController.java
+++ src/main/java/com/example/sushant/Endpoint/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +23,36 @@ public class UserController {
 		return mapper.findAll();
 	}
 	@GetMapping("/insert")
-	public List<Users> insert(){
+	public String insert(){
+		try {
 		Users user =new Users("ASHIBHA","Abhishek Bhapker","Ahmednagar");
 		mapper.insert(user);
+		return "Data Inserted Successfully..";
+		}catch(Exception e) {
+			return "BackEnd Problem"+e;
+		}
+
+	}  
+	@GetMapping("update")
+	public String update() {
+		try {
+			Users user =new Users("ASHIBHA","Abhishek Jadhav","Pune");
+			mapper.update(user);
+			return "Data Updates Successfully";
+		}catch(Exception e) {
+			return "BackEnd problem"+e;
+		}
+	}
 	
-		return mapper.findAll();
+	
+	@DeleteMapping("/delete")
+	public String delete() {
+		try {
+			String id="ASHIBHA";
+			mapper.delete(id);
+			return "Data deleted  Successfully";
+		}catch(Exception e) {
+			return "BackEnd problem : "+e;
+		}
 	}
 }
--- src/main/java/com/example/sushant/Mapper/UserMapper.java
+++ src/main/java/com/example/sushant/Mapper/UserMapper.java
@@ -2,9 +2,11 @@ package com.example.sushant.Mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.sushant.Model.Users;
 
@@ -16,4 +18,10 @@ public interface UserMapper {
 	
 	@Insert("insert into users values(#{id},#{name},#{address})")
 	void insert(Users user);
+	
+	@Update("update users set name=#{name},address=#{address} where id=#{id}")
+	void update(Users uers);
+	
+	@Delete("delete from users where id=#{id}")
+	void delete(String id);
 }
